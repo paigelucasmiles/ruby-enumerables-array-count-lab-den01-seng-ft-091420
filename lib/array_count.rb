57b12ef8f45ef70a04f7d133ea2681084a2abdac
Paige Miles <paige@lucas-miles.com>
2020-09-06 23:02:58 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,14 @@
+ # Return the total number of strings in the provided array using the count enumerable
+
 def count_strings(array)
-  # Return the total number of strings in the provided array using the count enumerable
+  array.count { |element| element.is_a? String }
 end
 
+
+
+
+# Return the total number of EMPTY strings in the provided array using the count enumerable
+
 def count_empty_strings(array)
-  # Return the total number of EMPTY strings in the provided array using the count enumerable
+  array.count { |element| element.is_a? String && element.empty? }
 end
